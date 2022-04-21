65959fee7b49e9152692dd2ec8de48142c36bee9
svanidzee <svanidzeee@gmail.com>
2022-04-21 17:07:19 +0100

    add theme file for chakra, add robot font,Feat: Prevent automatic page location restoration

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,34 @@
+import "@fontsource/roboto-slab";
+
 import Router from "next/router";
-import Head from "next/head";
 import NProgress from "nprogress";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import Layout from "../components/layout";
+import theme from "../lib/theme";
+
+// Prevent automatic page location restoration
+if (typeof window !== "undefined") {
+  window.history.scrollRestoration = "manual";
+}
 
 function MyApp({ Component, pageProps }) {
+  NProgress.configure({ showSpinner: false });
+
+  Router.events.on("routeChangeStart", () => {
+    NProgress.start();
+  });
+
+  Router.events.on("routeChangeComplete", () => {
+    NProgress.done();
+  });
+
   return (
-    <>
-      {/* <Head></Head> */}
-      <ChakraProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ChakraProvider>
-    </>
+    <ChakraProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
   );
 }
 
